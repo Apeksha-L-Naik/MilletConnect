910291348ed8b7a678f9441d3b05098ad1db86a8
chaithanyajgowda <chaithanyaj74@gmail.com>
2024-12-27 18:09:27 +0530

    changes

--- milletconnect/src/Components/AuthForm.js
+++ milletconnect/src/Components/AuthForm.js
@@ -60,7 +60,7 @@ function AuthForm() {
       className="min-h-screen flex justify-center items-center bg-cover bg-center"
       style={{
         backgroundImage:
-          'url(https://media.istockphoto.com/id/1419992346/vector/autumn-abstract-background-with-hand-drawn-ears-of-wheat.jpg?s=612x612&w=0&k=20&c=w94usonhzo5-btl1KaDnApG4Gb9BLuqg932o1tcJ6oA=)',
+          'url(https://i.pinimg.com/1200x/a9/a7/67/a9a76781fda00540bc484f56db320b80.jpg)',
       }}
     >
       <div className="bg-white bg-opacity-95 p-10 rounded-xl shadow-2xl w-96">
